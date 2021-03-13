ea852c8c17e64a62db309ab216f688bc86e837f5
Eric Meehan <eric.meehan@homodeus.us>
2021-03-13 09:35:16 -0500

    Created server_function in test

--- test/main.c
+++ test/main.c
@@ -14,7 +14,34 @@
 
 #include "../libeom.h"
 
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void * client_function(void *arg)
+{
+    
+}
+
+void * server_function(void *arg)
+{
+    struct Server server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 1248, 20);
+    struct sockaddr *address = (struct sockaddr *)&server.address;
+    socklen_t address_size = (socklen_t)sizeof(server.address);
+    while (1)
+    {
+        int client = accept(server.socket, address, &address_size);
+        char request[255];
+        memset(request, 0, 255);
+        read(client, request, 255);
+        printf("%s", request);
+        close(client);
+    }
+}
+
 int main()
 {
-    return 0;
+    struct ThreadPool thread_pool = thread_pool_constructor(11);
+    struct ThreadJob server_job = thread_job_constructor(server_function, NULL);
+    thread_pool.add_work(&thread_pool, server_job);
 }
